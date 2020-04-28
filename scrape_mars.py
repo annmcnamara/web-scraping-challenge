0fc5c205f3c48aa7a7849de2dd871de183048e3c
annmcnamara@gmail.com <annmcnamara@gmail.com>
2020-04-28 17:50:17 -0500

    cleaning up code

--- scrape_mars.py
+++ scrape_mars.py
@@ -10,45 +10,37 @@ def init_browser():
     # return Browser("chrome", **executable_path, headless=False)
     return Browser("chrome", "/usr/local/bin/chromedriver", headless=False)
 
-def scrape():
-    browser = init_browser()
-
-    # # SCRAPE THE LATEST NEWS FROM 'https://mars.nasa.gov/news/'
-    mars_data = {}
-
-    url = 'https://mars.nasa.gov/news/'
+def scrape_site(url, browser):
     browser.visit(url)
     time.sleep(5)
     # scrape and parse the data
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
+    return soup
+
+def scrape_news(mars_data, browser):
+    soup = scrape_site('https://mars.nasa.gov/news/', browser)
+
+    latest_news = soup.find('li', class_="slide")
 
     # Locate the object containing the news data
-    mars_data["headline"] = soup.find('div',class_="image_and_description_container").h3.text
-    mars_data["news_p"]   = soup.find('div',class_="image_and_description_container").get_text()
-    
-    # SCRAPE THE WEATHER FROM 'https://twitter.com/marswxreport?lang=en'
-    url = 'https://twitter.com/marswxreport?lang=en'
-    browser.visit(url)
-    time.sleep(5)
+    mars_data["headline"] = soup.find('li',class_="slide").h3.text
+    mars_data["news_p"]   = latest_news.find('div', class_= 'article_teaser_body').text
 
-    html = browser.html
-    soup = BeautifulSoup(html, 'html.parser')
+    return mars_data
+
+def scrape_twitter(mars_data, browser):
+    soup = scrape_site('https://twitter.com/marswxreport?lang=en', browser)
 
     tweet = soup.find('div',class_="css-901oao r-jwli3a r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0")
     mars_weather = tweet.text
 
     mars_data["weather"] = mars_weather
 
+    return mars_data
 
-    # SCRAPE THE IMAGE FROM 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-    browser.visit(url)
-    time.sleep(5)
-
-    # scrape and parse the data
-    html = browser.html
-    soup = BeautifulSoup(html, 'html.parser')
+def scrape_featured(mars_data, browser):
+    soup = scrape_site('https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars', browser)
 
     header = soup.find('div', class_="brand2")
     base_url ="http:"+header.a['href']
@@ -67,33 +59,26 @@ def scrape():
 
     mars_data["featured_image"] = featured_image_url
 
+    return mars_data
 
-    # SCRAPE THE TABLE DATA
-    ## set the url and open the web page
-    ## so we can scrape the data
+def scrape_table_data(mars_data, browser):
     url = 'https://space-facts.com/mars/'
     browser.visit(url)
     time.sleep(5)
-    tables = pd.read_html(url)
     
+    tables = pd.read_html(url)
 
     df = tables[0]
     df.columns = ['Attribute', 'Data']
 
-    #table_html = df.to_html(classes='table')
-
-    #table_html = re.sub("class=\"dataframe ", "class=\"", df.head(5).to_html(classes='table'))
-
     table_html = re.sub("border=\"1\" class=\"dataframe ", "class=\"", df.head(10).to_html(classes='table'))
     table_html = table_html.replace('\n', ' ')
-
-
-    print(table_html)
   
     mars_data['table'] = table_html
 
+    return mars_data
 
-    # SCRAPE THE HEMISPHERE IMAGES FROM 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+def scrape_hemisphere_images(mars_data, browser):
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
     driver = browser.driver
@@ -133,18 +118,32 @@ def scrape():
         dict_item = {'title':title, 'img_url':image_links[0].a['href']}
         hemisphere_image_urls.append(dict_item)
 
-    for i in hemisphere_image_urls:
-        print(i['title'])
-        print(i['img_url'])
-
 
     mars_data["hemisphere_image_urls"]    = hemisphere_image_urls
 
-    # print (f" CHECK {mars_data.hemisphere_image_urls[0]} \n")
+    return mars_data
 
-    print("SCRAPED :-) \n\n")
+def scrape():
+    browser = init_browser()
+
+    mars_data = {}
+
+    # # SCRAPE THE LATEST NEWS FROM 'https://mars.nasa.gov/news/'
+    mars_data = scrape_news(mars_data,browser)
+    
+    # SCRAPE THE WEATHER FROM 'https://twitter.com/marswxreport?lang=en'
+    mars_data = scrape_twitter(mars_data, browser)
+
+    # SCRAPE THE IMAGE FROM 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    mars_data = scrape_featured(mars_data, browser)
+  
+    # SCRAPE THE TABLE DATA 'https://space-facts.com/mars/'
+    mars_data = scrape_table_data(mars_data, browser)
+
+    # SCRAPE THE HEMISPHERE IMAGES FROM 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    mars_data = scrape_hemisphere_images(mars_data, browser)
+
     return mars_data 
-
 
 
 # To enable testingJ
